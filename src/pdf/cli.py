5fe54b2a42736dc50727cb3c85150ac79008c261
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-10-24 10:06:36 +0300

    added pdf converter to docx

--- src/pdf/cli.py
+++ src/pdf/cli.py
@@ -3,6 +3,7 @@ import typer
 
 from pdf.converter import convert_pdf_to_docx, convert_pdf_to_selectable
 from common.output import show_message
+from typing import Optional
 
 app = typer.Typer()
 
@@ -10,16 +11,19 @@ app = typer.Typer()
 @app.command()
 @click.argument('input_file')
 @click.argument('output_file')
-def convert_pdf_to_selectable_pdf(input_file: str, output_file: str):
+def convert_pdf_to_selectable_pdf(input_file: str, output_file: Optional[str] = typer.Argument(None)):
     """Converts a scan PDF file to selectable PDF."""
     convert_pdf_to_selectable(input_file, output_file)
     show_message("File converted successfully[green].")
 
 
 @app.command()
-def convert_to_docx():
+@click.argument('input_file')
+@click.argument('output_file')
+def convert_to_docx(input_file: str, output_file: Optional[str] = typer.Argument(None)):
     """Converts a PDF file to a DOCX file."""
-    show_message("Converted to DOCX")
+    convert_pdf_to_docx(input_file, output_file)
+    show_message("File converted successfully[green].")
 
 
 typer_click_object = typer.main.get_command(app)
--- src/pdf/converter.py
+++ src/pdf/converter.py
@@ -1,14 +1,27 @@
 import sys
 import ocrmypdf
 from common.output import show_message
+from common.file import get_filename
+from pdf2docx import Converter
 
 
-def convert_pdf_to_selectable(input_file: str, output_file: str) -> None:
+def convert_pdf_to_selectable(input_file: str, output_file) -> None:
+    if output_file is None:
+        output_file = "{0}_selectable.pdf".format(get_filename(input_file))
     try:
         ocrmypdf.ocr(input_file, output_file, deskew=True)
     except Exception as err:
         show_message('Error converting to selectable pdf: {0}'.format(err))
 
 
-def convert_pdf_to_docx():
-    pass
+def convert_pdf_to_docx(pdf_file: str, docx_file: str):
+    if docx_file is None:
+        docx_file = "{0}.docx".format(get_filename(pdf_file))
+    selectable_pdf = "{0}_selectable.pdf".format(get_filename(pdf_file))
+    convert_pdf_to_selectable(pdf_file, selectable_pdf)
+    try:
+        cv = Converter(selectable_pdf)
+        cv.convert(docx_file, multi_processing=True)
+        cv.close()
+    except Exception as err:
+        show_message('Error converting to docx: {0}'.format(err))
