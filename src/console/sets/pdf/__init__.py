31166e7c91f3882d97bf4f90a663602af04bac1a
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-10-28 20:54:46 +0300

    moved pdf app to sets

---
+++ src/console/sets/pdf/__init__.py
@@ -0,0 +1,4 @@
+from console.sets.pdf.cli import app as pdf_app
+
+__version__ = "0.0.1"
+__all__ = ["pdf_app"]
--- src/pdf/__init__.py
+++
@@ -1,4 +0,0 @@
-from .converter import convert_pdf_to_docx, convert_pdf_to_selectable
-
-__version__ = "0.0.1"
-__all__ = ["convert_pdf_to_docx", "convert_pdf_to_selectable"]
