34ba9d4ad100b73ba58baab9f3e8b7347b7c02b1
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-03 22:05:15 +0200

    extra output methods

--- src/common/output.py
+++ src/common/output.py
@@ -1,7 +1,27 @@
 from rich.console import Console
+import typer
 
 console = Console()
 
+ERROR_STYLE = "bold red"
+INFO_STYLE = "bold blue"
+SUCCESS_STYLE = "bold green"
 
-def show_message(message: str) -> None:
-    console.print(message)
+
+def success_message(message: str) -> None:
+    show_message(message=message, style=SUCCESS_STYLE)
+
+
+def error_message(message: str) -> None:
+    show_message(message=message, style=ERROR_STYLE)
+
+
+def show_message(message: str, style: str = None) -> None:
+    if style is None:
+        style = INFO_STYLE
+    console.print(message, style=style)
+
+
+def abort(message: str):
+    error_message(message=message)
+    raise typer.Abort()
