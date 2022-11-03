fb69a0e796d1ba135a95950dbbe4938282fb548b
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-03 22:04:30 +0200

    toml file support

--- src/common/file.py
+++ src/common/file.py
@@ -1,5 +1,11 @@
 import os
+import tomli
 
 
 def get_filename(file: str) -> str:
     return os.path.splitext(file)[0]
+
+
+def load_toml(path: str):
+    with open(path, mode='rb') as toml_file:
+        return tomli.load(toml_file)
