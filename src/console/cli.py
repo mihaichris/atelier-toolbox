7363f8b92ff735c9b62df1c8175ed5ccb52245a6
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-15 20:32:50 +0200

    remove dm subcommand

--- src/console/cli.py
+++ src/console/cli.py
@@ -1,16 +1,13 @@
-import click
 import typer
 
-import console.sets.pdf.cli as pdf
-import console.sets.business.cli as business
-import console.sets.dm.cli as dm
+from console.sets.pdf import pdf_app
+from console.sets.business import business_app
 
 app = typer.Typer(name="Tools for various automations.")
-app.add_typer(pdf.app, name="pdf")
-app.add_typer(business.app, name="business")
-app.add_typer(dm.app, name="dm")
-typer_click_object = typer.main.get_command(app)
+app.add_typer(pdf_app, name="pdf")
+app.add_typer(business_app, name="business")
+command = typer.main.get_command(app)
 
 
 def main():
-    typer_click_object()
+    command()
--- src/console/sets/dm/__init__.py
+++
@@ -1,4 +0,0 @@
-from console.sets.dm.cli import app as dm_app
-
-__version__ = "0.0.1"
-__all__ = ["dm_app"]
