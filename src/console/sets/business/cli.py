2520d9f67fee9a039c35a81e5f364f1faca4d189
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-17 21:16:43 +0200

    removed today flag for getting working hours

--- src/console/sets/business/cli.py
+++ src/console/sets/business/cli.py
@@ -9,12 +9,9 @@ app = typer.Typer(name="Business CLI Tool")
 
 
 @app.command()
-def working_hours(is_today: bool = typer.Option(False, '--today', help="Get current day working hours.", is_flag=True),
-                  is_month: bool = typer.Option(False, '--this-month', help="Get current month working hours.", is_flag=True)):
+def working_hours(is_month: bool = typer.Option(False, '--this-month', help="Get current month working hours.", is_flag=True)):
     """Get working hours based of a date period."""
     year = datetime.now().year
-    if is_today:
-        show_message("Current day working hours: {0}".format(8))
     if is_month:
         month = datetime.now().month
         month_working_hours = get_month_working_hours(month, year)
