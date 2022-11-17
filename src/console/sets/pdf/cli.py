04201bc750fb8808881d54a1e3f1f3a6be83b5cb
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-17 21:14:55 +0200

    add  error handling in the command

--- src/console/sets/pdf/cli.py
+++ src/console/sets/pdf/cli.py
@@ -2,21 +2,27 @@ import click
 import typer
 
 from console.sets.pdf.converter import convert_pdf_to_docx, convert_pdf_to_selectable
-from common.output import show_message
+from common.output import show_message, abort
 from typing import Optional
 
 app = typer.Typer(name="PDF CLI Tool")
 
 
 @app.command()
-def convert_pdf_to_selectable_pdf(input_file: str, output_file: Optional[str] = typer.Argument(None)):
+def convert_to_selectable_pdf(input_file: str, output_file: Optional[str] = typer.Argument(None)):
     """Converts a scan PDF file to selectable PDF."""
-    convert_pdf_to_selectable(input_file, output_file)
+    try:
+        convert_pdf_to_selectable(input_file, output_file)
+    except Exception as err:
+        abort('Error converting to selectable pdf: {0}'.format(err))
     show_message("File converted successfully[green].")
 
 
 @app.command()
 def convert_to_docx(input_file: str, output_file: Optional[str] = typer.Argument(None)):
     """Converts a PDF file to a DOCX file."""
-    convert_pdf_to_docx(input_file, output_file)
+    try:
+        convert_pdf_to_docx(input_file, output_file)
+    except Exception as err:
+        abort('Error converting to docx: {0}'.format(err))
     show_message("File converted successfully[green].")
--- src/console/sets/pdf/converter.py
+++ src/console/sets/pdf/converter.py
@@ -1,4 +1,3 @@
-import sys
 import ocrmypdf
 from common.output import show_message
 from common.file import get_filename
@@ -8,10 +7,7 @@ from pdf2docx import Converter
 def convert_pdf_to_selectable(input_file: str, output_file) -> None:
     if output_file is None:
         output_file = "{0}_selectable.pdf".format(get_filename(input_file))
-    try:
-        ocrmypdf.ocr(input_file, output_file, deskew=True)
-    except Exception as err:
-        show_message('Error converting to selectable pdf: {0}'.format(err))
+    ocrmypdf.ocr(input_file, output_file, deskew=True)
 
 
 def convert_pdf_to_docx(pdf_file: str, docx_file: str):
@@ -19,9 +15,6 @@ def convert_pdf_to_docx(pdf_file: str, docx_file: str):
         docx_file = "{0}.docx".format(get_filename(pdf_file))
     selectable_pdf = "{0}_selectable.pdf".format(get_filename(pdf_file))
     convert_pdf_to_selectable(pdf_file, selectable_pdf)
-    try:
-        cv = Converter(selectable_pdf)
-        cv.convert(docx_file, multi_processing=True)
-        cv.close()
-    except Exception as err:
-        show_message('Error converting to docx: {0}'.format(err))
+    cv = Converter(selectable_pdf)
+    cv.convert(docx_file, multi_processing=True)
+    cv.close()
