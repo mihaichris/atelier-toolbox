ebecb71b8a1ac952bc30ebd7a05c97ba12337df7
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-19 19:12:28 +0200

    added docstring to tests

--- tests/console/test_business_commands.py
+++ tests/console/test_business_commands.py
@@ -1,24 +1,32 @@
+"""Module for testing Business commands."""
 import unittest
 from typer.testing import CliRunner
-
 from src.console.cli import app
+from src.common.work import get_month_working_days, get_month_working_hours
 
 runner = CliRunner()
 
 
 class TestBusinessCommands(unittest.TestCase):
+    """Testing class for Business commands"""
 
-    def test_get_working_days_today(self):
+    def test_get_working_days_current_month(self):
+        """Test get working days current month."""
         result = runner.invoke(
             app, ["business", "working-days", "--this-month"])
+        days = get_month_working_days(11, 2022)
         self.assertEqual(0, result.exit_code)
-        self.assertRegex(result.output, r'(Current month working days|22)')
+        self.assertRegex(
+            result.output, rf'(Current month working days|{days})')
 
-    def test_get_working_hours_today(self):
+    def test_get_working_hours_this_month(self):
+        """Test get working hours current month."""
         result = runner.invoke(
             app, ["business", "working-hours", "--this-month"])
+        hours = get_month_working_hours(11, 2022)
         self.assertEqual(0, result.exit_code)
-        self.assertRegex(result.output, r'(Current month working hours|176)')
+        self.assertRegex(
+            result.output, rf'(Current month working hours|{hours})')
 
 
 if __name__ == '__main__':
--- tests/console/test_pdf_commands.py
+++ tests/console/test_pdf_commands.py
@@ -1,15 +1,17 @@
+"""Module for testing PDF commands."""
 import unittest
-from typer.testing import CliRunner
 import os
-
+from typer.testing import CliRunner
 from src.console.cli import app
 
 runner = CliRunner()
 
 
 class TestPDFCommands(unittest.TestCase):
+    """Testing class for PDF commands"""
 
     def test_convert_selectable_pdf_to_selectable_pdf_without_output_will_abort(self):
+        """Test if convert a selectable pdf to selectable pdf will output error."""
         result = runner.invoke(
             app, ["pdf", "convert-to-selectable-pdf", "tests/resources/selectable_test.pdf"])
         self.assertEqual(1, result.exit_code)
@@ -18,8 +20,9 @@ class TestPDFCommands(unittest.TestCase):
             'tests/resources/scanned_test_selectable.pdf'))
 
     def test_convert_pdf_to_selectable_pdf_without_output(self):
-        result = runner.invoke(
-            app, ["pdf", "convert-to-selectable-pdf", "tests/resources/scanned_test.pdf"])
+        """Test converting a scanned pdf to selectable pdf with no specified output file."""
+        result = self.invoke_convert_to_selectable_pdf(
+            ["tests/resources/scanned_test.pdf"])
         self.assertEqual(0, result.exit_code)
         self.assertRegex(result.output, r'(File converted successfully)')
         self.assertTrue(os.path.isfile(
@@ -27,8 +30,9 @@ class TestPDFCommands(unittest.TestCase):
         os.remove('tests/resources/scanned_test_selectable.pdf')
 
     def test_convert_pdf_to_selectable_pdf_with_output(self):
-        result = runner.invoke(
-            app, ["pdf", "convert-to-selectable-pdf", "tests/resources/scanned_test.pdf", "tests/resources/scanned_test_output.pdf"])
+        """Test converting a scanned pdf to selectable pdf with specified output file."""
+        result = self.invoke_convert_to_selectable_pdf(
+            ["tests/resources/scanned_test.pdf", "tests/resources/scanned_test_output.pdf"])
         self.assertEqual(0, result.exit_code)
         self.assertRegex(result.output, r'(File converted successfully)')
         self.assertTrue(os.path.isfile(
@@ -36,22 +40,35 @@ class TestPDFCommands(unittest.TestCase):
         os.remove('tests/resources/scanned_test_output.pdf')
 
     def test_convert_pdf_to_docx_without_output(self):
-        result = runner.invoke(
-            app, ["pdf", "convert-to-docx", "tests/resources/scanned_test.pdf"])
+        """Test converting a scanned pdf to docx with no specified output file."""
+        result = self.invoke_convert_to_docx_pdf(
+            ["tests/resources/scanned_test.pdf"])
         self.assertEqual(0, result.exit_code)
         self.assertRegex(result.output, r'(File converted successfully)')
-        self.assertTrue(os.path.isfile(
-            'tests/resources/scanned_test.docx'))
+        self.assertTrue(os.path.isfile('tests/resources/scanned_test.docx'))
         os.remove('tests/resources/scanned_test.docx')
 
     def test_convert_pdf_to_docx_with_output(self):
-        result = runner.invoke(
-            app, ["pdf", "convert-to-docx", "tests/resources/scanned_test.pdf", "tests/resources/scanned_test_output.docx"])
+        """Test converting a scanned pdf to docx with specified output file."""
+        result = self.invoke_convert_to_docx_pdf(
+            ["tests/resources/scanned_test.pdf", "tests/resources/scanned_test_output.docx"])
         self.assertEqual(0, result.exit_code)
         self.assertRegex(result.output, r'(File converted successfully)')
         self.assertTrue(os.path.isfile(
             'tests/resources/scanned_test_output.docx'))
         os.remove('tests/resources/scanned_test_output.docx')
+
+    def invoke_convert_to_selectable_pdf(self, args):
+        """Invoke the convert-to-selectable-pdf command with args."""
+        commands = ["pdf", "convert-to-selectable-pdf"]
+        commands.extend(args)
+        return runner.invoke(app, commands)
+
+    def invoke_convert_to_docx_pdf(self, args):
+        """Invoke the convert-to-docx command with args."""
+        commands = ["pdf", "convert-to-docx"]
+        commands.extend(args)
+        return runner.invoke(app, commands)
 
 
 if __name__ == '__main__':
