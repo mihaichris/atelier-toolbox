97964e2b6564dd131343e88cf732a00495b19864
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-21 10:22:56 +0200

    refactor handling files and added docstrings to file packages

--- tests/common/test_calendar.py
+++ tests/common/test_calendar.py
@@ -1,42 +1,49 @@
+"""Module for testing the calendar package."""
 import unittest
+import datetime
 from src.common.calendar import get_first_date_of_month, get_last_date_of_month
-from datetime import datetime
 
 
 class TestCalendar(unittest.TestCase):
+    """Testing class for calendar."""
 
     def test_get_first_date_of_current_month(self):
-        now = datetime.now()
-        first_date = datetime(now.year, now.month, 1)
+        """Test get first date of current month."""
+        now = datetime.datetime.now()
+        first_date = datetime.datetime(now.year, now.month, 1)
         current_month_first_date = get_first_date_of_month(
             now.year, now.month)
         self.assertEqual(first_date.strftime(
             "%Y-%m-%d"), current_month_first_date)
 
     def test_get_first_date_of_past_month(self):
-        now = datetime.now()
-        last_date = datetime(now.year, now.month - 1, 1)
+        """Test get first date of past month."""
+        now = datetime.datetime.now()
+        last_date = datetime.datetime(now.year, now.month - 1, 1)
         last_month_first_date = get_first_date_of_month(
             now.year, now.month-1)
         self.assertEqual(last_date.strftime("%Y-%m-%d"), last_month_first_date)
 
     def test_get_first_date_of_future_month(self):
-        now = datetime.now()
-        future_date = datetime(now.year, now.month + 1, 1)
+        """Test get first date of future month."""
+        now = datetime.datetime.now()
+        future_date = datetime.datetime(now.year, now.month + 1, 1)
         future_month_first_date = get_first_date_of_month(
             now.year, now.month+1)
         self.assertEqual(future_date.strftime(
             "%Y-%m-%d"), future_month_first_date)
 
-    def test_get_last_date_January(self):
-        last_date = datetime(2022, 1, 31)
+    def test_get_last_date_january(self):
+        """Test get last date of January."""
+        last_date = datetime.datetime(2022, 1, 31)
         january_last_date = get_last_date_of_month(
             2022,  1)
         self.assertEqual(last_date.strftime(
             "%Y-%m-%d"), january_last_date)
 
     def test_get_last_date_of_december(self):
-        last_date = datetime(2022, 12, 31)
+        """Test get last date of december."""
+        last_date = datetime.datetime(2022, 12, 31)
         last_month_december = get_last_date_of_month(
             2022, 12)
         self.assertEqual(last_date.strftime("%Y-%m-%d"), last_month_december)
--- tests/common/test_file.py
+++ tests/common/test_file.py
@@ -1,26 +1,35 @@
+"""Module for testing the file package."""
 import unittest
 import os
-import src.common.file as file
+from src.common import file
 
 
 class TestFile(unittest.TestCase):
+    """Test class for file package."""
 
     def test_get_filename(self):
+        """Test get filename."""
         current_filename = 'test_file.py'
         self.assertEqual('test_file', file.get_filename(current_filename))
 
     def test_load_toml_file(self):
+        """Test load toml file."""
         self.init_test_file()
         self.assertEqual({'test': {'value': 1}},
                          file.load_toml('test_toml_file.toml'))
         self.remove_test_file()
 
     def init_test_file(self):
-        toml_file = open("test_toml_file.toml", 'w')
-        toml_file.write("[test]\nvalue=1")
-        toml_file.close()
+        """Init test file."""
+        try:
+            with open("test_toml_file.toml", 'w',  encoding="utf-8") as toml_file:
+                toml_file.write("[test]\nvalue=1")
+                toml_file.close()
+        except OSError:
+            toml_file = None
 
     def remove_test_file(self):
+        """Remove test file."""
         os.remove('test_toml_file.toml')
 
 
