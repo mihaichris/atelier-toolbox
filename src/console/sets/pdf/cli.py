99d2d14088d2cd890e4aa7e2a832d306292ca352
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-11-21 10:22:00 +0200

    raise custom exceptions for pdf cli

--- src/console/sets/pdf/cli.py
+++ src/console/sets/pdf/cli.py
@@ -1,9 +1,9 @@
-import click
+"""PDF set commads."""
+from typing import Optional
 import typer
-
 from console.sets.pdf.converter import convert_pdf_to_docx, convert_pdf_to_selectable
 from common.output import show_message, abort
-from typing import Optional
+from common.exceptions import ConvertPDFException
 
 app = typer.Typer(name="PDF CLI Tool")
 
@@ -13,16 +13,17 @@ def convert_to_selectable_pdf(input_file: str, output_file: Optional[str] = type
     """Converts a scan PDF file to selectable PDF."""
     try:
         convert_pdf_to_selectable(input_file, output_file)
-    except Exception as err:
-        abort('Error converting to selectable pdf: {0}'.format(err))
+    except ConvertPDFException as err:
+        print(err)
+        abort(f'Error converting to selectable pdf: {err}')
     show_message("File converted successfully[green].")
 
 
-@app.command()
+@ app.command()
 def convert_to_docx(input_file: str, output_file: Optional[str] = typer.Argument(None)):
     """Converts a PDF file to a DOCX file."""
     try:
         convert_pdf_to_docx(input_file, output_file)
-    except Exception as err:
-        abort('Error converting to docx: {0}'.format(err))
+    except ConvertPDFException as err:
+        abort(f'Error converting to docx: {err}')
     show_message("File converted successfully[green].")
