6301e17a798e3dd2aa50608f933a5b07a2b03485
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-01-10 22:24:45 +0200

    [#40] added download dedicated exception

--- src/toolbox/common/exceptions.py
+++ src/toolbox/common/exceptions.py
@@ -8,3 +8,11 @@ class ConvertPDFException(Exception):
         """ConvertPDFException constructor."""
         super().__init__()
         self.message = message
+
+class DownloadException(Exception):
+    """ConvertPDFException class."""
+
+    def __init__(self, message):
+        """ConvertPDFException constructor."""
+        super().__init__()
+        self.message = message
