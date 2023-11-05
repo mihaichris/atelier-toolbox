7e5acf51fd430c0e7bff497f38f2fa15e54ad90c
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-11-05 19:37:13 +0200

    removed tests from pdf

--- src/toolbox/sets/pdf/cli.py
+++ src/toolbox/sets/pdf/cli.py
@@ -1,8 +1,4 @@
 """PDF set commads."""
-from typing import Optional
 import typer
-from toolbox.sets.pdf.converter import convert_pdf_to_docx, convert_pdf_to_selectable
-from toolbox.common.output import show_message, abort
-from toolbox.common.exceptions import ConvertPDFException
 
 app = typer.Typer(short_help="PDF CLI Tool")
